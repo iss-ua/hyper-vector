2629ec3ae2e6d077e254819285fdbf9260201fed
Dmitry Zhora <dmitry.zhora@gmx.com>
2023-08-05 23:50:20 +0300

    Benchmarking the random base vector generation

---
+++ HyperVector/Core.Tests/BenchmarkTests.cs
@@ -0,0 +1,74 @@
+using Xunit.Abstractions;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+
+namespace HyperVector.Core.Tests
+{
+#pragma warning disable CS8981
+	using half = System.Half;
+#pragma warning restore
+
+	[RankColumn, MemoryDiagnoser]
+	public class BenchmarkTests
+	{
+		private readonly ITestOutputHelper? _outputHelper;
+
+		// The BenchmarkDotNet framework does not provide an injection
+		public BenchmarkTests(ITestOutputHelper? outputHelper = null)
+		{
+			_outputHelper = outputHelper;
+		}
+
+		[Fact(Skip = "This method is time consuming")]
+		public void StartBenchmarkTests()
+		{
+			var accumulationlogger = new AccumulationLogger();
+			var manualConfig = ManualConfig.Create(DefaultConfig.Instance)
+				.WithOptions(ConfigOptions.DisableOptimizationsValidator)
+				.AddLogger(accumulationlogger);
+
+			BenchmarkRunner.Run<BenchmarkTests>(manualConfig);
+			if (_outputHelper != null)
+				_outputHelper.WriteLine(accumulationlogger.GetLog());
+		}
+
+		[Fact, Benchmark]
+		public void BenchmarkHalfArray()
+		{
+			var halfVector = DataVector<half>.GenerateBaseVector(4096, (half) 0.1f);
+
+			if (_outputHelper != null)
+			{
+				_outputHelper.WriteLine
+					($"Generated half base vector of size {halfVector.VectorSize}");
+			}
+		}
+
+		[Fact, Benchmark]
+		public void BenchmarkFloatArray()
+		{
+			var floatVector = DataVector<float>.GenerateBaseVector(4096, 0.1f);
+
+			if (_outputHelper != null)
+			{
+				_outputHelper.WriteLine
+					($"Generated float base vector of size {floatVector.VectorSize}");
+			}
+		}
+
+		[Fact, Benchmark]
+		public void BenchmarkDoubleArray()
+		{
+			var doubleVector = DataVector<double>.GenerateBaseVector(4096, 0.1);
+
+			if (_outputHelper != null)
+			{
+				_outputHelper.WriteLine
+					($"Generated double base vector of size {doubleVector.VectorSize}");
+			}
+		}
+	}
+}
--- HyperVector/Core.Tests/DataVectorTests.cs
+++ HyperVector/Core.Tests/DataVectorTests.cs
@@ -10,16 +10,26 @@ namespace HyperVector.Core.Tests
 		public void VectorCreationTests()
 		{
 			var halfVector = new DataVector<half>(4096);
+			Assert.NotNull(halfVector);
+
 			var floatVector = new DataVector<float>(4096);
+			Assert.NotNull(floatVector);
+
 			var doubleVector = new DataVector<double>(4096);
+			Assert.NotNull(doubleVector);
 		}
 
 		[Fact]
 		public void VectorGenerationTests()
 		{
 			var halfVector = DataVector<half>.GenerateBaseVector(4096, (half) 0.1f);
+			Assert.NotNull(halfVector);
+
 			var floatVector = DataVector<float>.GenerateBaseVector(4096, 0.1f);
+			Assert.NotNull(floatVector);
+
 			var doubleVector = DataVector<double>.GenerateBaseVector(4096, 0.1);
+			Assert.NotNull(doubleVector);
 		}
 	}
 }
